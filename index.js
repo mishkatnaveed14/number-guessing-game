0cdc23021d2fdd4c2ac4bf9f9385152cd1d7e1e9
Mishkat Naveed <mishkatnaveed14@gmail.com>
2026-06-30 14:30:15 +0500

    working

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 // ///dark mode
-console.log("js working");
-
 let btn = document.getElementById("theme_btn")
 btn.addEventListener("click",()=>{
   document.body.classList.toggle("dark_mode")
 
 })
-// 
+// easy k onclick pr kam 
+let mode = document.querySelector(".mode")
+// console.log(easy_mode);
+mode.addEventListener("click",function () {
+  if (mode ===) {
+    
+  }
+})
